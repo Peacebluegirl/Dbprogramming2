0e07069d546718d612629ef8c4d93596d2b23b3e
Peacebluegirl <110713148+Peacebluegirl@users.noreply.github.com>
2023-05-08 17:06:00 +0700

    Add files via upload

---
+++ dbprogramming2/src/dbprogramming2/LAB9.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dbprogramming2;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class LAB9 {
+
+    public static void main(String[] args) {
+        String URL = "jdbc:mysql://localhost:3306/mydb2";
+        String username = "root";
+        String password = "1234";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(URL, username, password);
+            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO student(studentID,firstName,lastName,email,deptID)"
+                    + "VALUES('111111','AAAA','BBBB','aaaa@gmail.com','IT')";
+            statement.executeUpdate(sql);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LAB9.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(LAB9.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
